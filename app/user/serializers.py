47237d59dd75430a4dfc09e0f6263eec87e9b71c
Mo Abdelazim <mo.alazim@gmail.com>
2022-10-29 09:42:51 +0400

    fixing typo

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -14,7 +14,7 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
         fields = ['email', 'password', 'name']
-        extra_kwargs = {'password': {'write_onley': True, 'min_length': 5}}
+        extra_kwargs = {'password': {'write_only': True, 'min_length': 5}}
 
     def create(self, validated_data):
         '''Created and return a user with encrypted password.'''
