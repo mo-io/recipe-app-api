f4e944a295fabd05a667a6d2fbcd996505c99611
Mo Abdelazim <mo.alazim@gmail.com>
2022-10-29 09:37:58 +0400

    user auth token and serializer

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -24,11 +24,10 @@ class AdminSiteTests(TestCase):
         )
         return super().setUp()
 
+    def test_users_list(self):
+        '''Test that users are listed on page'''
+        url = reverse('admin:core_user_changelist')
+        res = self.client.get(url)
 
-def test_users_list(self):
-    '''Test that users are listed on page'''
-    url = reverse('admin:core_user_changelist')
-    res = self.client.get(url)
-
-    self.assertContains(res, self.user.name)
-    self.assertContains(res, self.user.email)
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
